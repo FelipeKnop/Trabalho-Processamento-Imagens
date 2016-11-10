49d9b85406be91b1d382eba4f9d0850a439572e2
FelipeKnop <felipebarraknop@yahoo.com.br>
2016-11-10 00:28:21 -0200

    Funções iniciais implementadas (abrir imagem, exibir imagem e borrar imagem)

--- trabalhopi.py
+++ trabalhopi.py
@@ -1,15 +1,54 @@
 import click
 import matplotlib.pyplot as plt
 from scipy import misc
+from scipy import ndimage
+
+from structure import cli
+from structure import generator
+from structure import processor
 
 
-@click.command('open')
-@click.option('-i', '--image', help='Imagem a ser processada.')
-def mostra(image):
-    face = misc.imread(image)
-    plt.imshow(face)
-    plt.show()
+@cli.command('open')
+@click.option('-i', '--image', 'images', type=click.Path(),
+              multiple=True, help='Imagem a ser aberta.')
+@generator
+def open_cmd(images):
+    """"Carrega uma ou mais imagens para processamento. O parâmetro de entrada
+    pode ser especificado várias vezes para carregar mais de uma imagem."""
+    for image in images:
+        try:
+            click.echo('Abrindo "%s"' % image)
+            img = misc.imread(image)
+            yield img
+        except Exception as e:
+            click.echo('Imagem não pode ser aberta "%s": %s' % (image, e), err=True)
+
+@cli.command('blur')
+@click.option('-s', '--sigma', default=1, type=int,
+              help='Sigma do filtro gaussiano.', show_default=True)
+@processor
+def blur_cmd(images, sigma):
+    """"Borra a imagem usando o filtro gaussiano com SIGMA passado
+    como parâmetro."""
+    for image in images:
+        try:
+            click.echo('Aplicando filtro gaussiano com sigma %s' % sigma)
+            blurred_img = ndimage.gaussian_filter(image, sigma=(sigma, sigma, 1))
+            yield blurred_img
+        except Exception as e:
+            click.echo('Filtro não pode ser aplicado "%s": %s' % (image, e), err=True)
+
+
+@cli.command('display')
+@processor
+def display_cmd(images):
+    """Abre todas as imagens em um visualizador de imagens."""
+    for image in images:
+        click.echo('Exibindo imagem')
+        plt.imshow(image)
+        plt.show()
+        yield image
 
 
 if __name__ == "__main__":
-    mostra()
+    cli()
